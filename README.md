fbfbfd1faea8659bdb1ca34ea8f000968888ae8b
FraSharp <s23265@iisve.it>
2022-08-07 10:14:48 +0200

    init
    Signed-off-by: FraSharp <s23265@iisve.it>

---
+++ README.md
@@ -0,0 +1,3 @@
+# Coffee -- wezterm
+
+Coffee is a nice, warm colour scheme for software based of gruvbox which is light on eye strain and feels warm and cozy -- wezterm
---
+++ wezterm.lua
@@ -0,0 +1,17 @@
+local wezterm = require 'wezterm';
+
+return {
+	colors = {
+		foreground      = "#a89984",
+		background      = "#303030",
+		cursor_bg       = "#bbbbbb",
+		cursor_border   = "#8f9494",
+		selection_fg    = "#303030",
+		selection_bg    = "#87afaf",
+		scrollbar_thumb = "#8f9494",
+
+		--	    Black      Red	  Green	     Yellow     Blue       Magenta    Cyan       White
+		ansi    = {"#262220", "#af5f5f", "#87875f", "#bcbc6c", "#666c7f", "#cd7998", "#6d978a", "#bbbbbb"},
+		brights = {"#8f9494", "#bb6868", "#849155", "#c5c563", "#87afaf", "#c987a0", "#7ca296", "#ddd0c0"},
+	},
+}
